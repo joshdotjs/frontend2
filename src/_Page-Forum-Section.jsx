b584d9faf2e01398c1832e032a27a853018f9fc4
joshdotjs <joshdotjs@gmail.com>
2024-04-23 23:58:37 -0500

    create thread inside a modal

--- src/_Page-Forum-Section.jsx
+++ src/_Page-Forum-Section.jsx
@@ -17,6 +17,23 @@ import { http } from './util/http';
 import { apiUrl } from './util/url';
 import { asynch } from './util/async';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 // ==============================================
 // ==============================================
 // ==============================================
@@ -36,6 +53,10 @@ export default function ForumSectionPage () {
 
   const [title, setTitle] = useState('New Thread');
   const [content, setContent] = useState('New Content');
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   // ============================================
 
@@ -125,31 +146,53 @@ export default function ForumSectionPage () {
       </ul>
 
 
+      {/* Create New Thread */}
+      {/* Create New Thread */}
+      {/* Create New Thread */}
+      {/* Create New Thread */}
       <div>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        >
-        </input>
-        <textarea
-          type="text"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
 
-        <button
-          onClick={() => {
-            if (!user?.logged_in) {
-              notify({message: 'Please log in to create a thread...', variant: 'warning', duration: 3000})();
-              return navigate('/auth/login');
-            }
-              
-            createThread();
-          }}
-        >
-          Create Thread
-        </button>
+        <div>
+          <Button onClick={handleOpen}>Create Thread</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              >
+              </input>
+              <textarea
+                type="text"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+
+              <button
+                onClick={() => {
+                  if (!user?.logged_in) {
+                    notify({message: 'Please log in to create a thread...', variant: 'warning', duration: 3000})();
+                    return navigate('/auth/login');
+                  }
+                    
+                  setOpen(false);
+                  createThread();
+                }}
+              >
+                Create Thread
+              </button>
+
+            </Box>
+
+          </Modal>
+        </div>
+
       </div>
       
     </Layout>
