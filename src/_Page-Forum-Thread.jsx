a05e96e750035b136c24e721112b00976c597bbe
joshdotjs <joshdotjs@gmail.com>
2024-04-30 22:50:31 -0500

    edit modal

--- src/_Page-Forum-Thread.jsx
+++ src/_Page-Forum-Thread.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 // comps:
 import Layout from './_layout';
@@ -39,6 +43,23 @@ export default function ForumThreadPage () {
   const navigate = useNavigate();
   const [ notify ] = useNotification();
 
+  // ============================================
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   // ============================================
 
@@ -128,6 +149,27 @@ export default function ForumThreadPage () {
                   </Link>
                 }
               </h6>
+
+              { user.id === post.user_id &&
+                <>
+                  <Button onClick={handleOpen}>Edit</Button>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                      </Typography>
+                    </Box>
+                  </Modal>
+                </>
+              }
             </li>
           );
         })}
