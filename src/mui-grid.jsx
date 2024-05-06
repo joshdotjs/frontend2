72862ead1a184ed24b3971d5e8190aac0d36508b
joshdotjs <joshdotjs@gmail.com>
2024-05-05 21:53:03 -0500

    responsive

--- src/mui-grid.jsx
+++ src/mui-grid.jsx
@@ -10,23 +10,29 @@ import { useTheme } from '@mui/material/styles';
 
 // ==============================================
 
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
+
+// ==============================================
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-}));
 
-// ==============================================
-
-const box_css = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   color: 'white',
   padding: 2
-};
+}));
 
 // ==============================================
 // ==============================================
@@ -52,25 +58,29 @@ export default () => {
   const lg = useMediaQuery(sizes.lg);
   const xl = useMediaQuery(sizes.xl);
 
+  let box_css = {
+    A: { background: 'deepskyblue' },
+    B: { background: 'tomato' },
+    C: { background: 'darkorange' },
+  };
+  if (md) {
+    box_css = {
+      A: { ...box_css.A, gridRow: '1 / -1', gridColumn: '1 / 2',  },
+      B: { ...box_css.B, gridRow: '1 / 2',  gridColumn: '2 / 3',   },
+      C: { ...box_css.C, gridRow: '2 / 3',  gridColumn: '2 / 3',   },
+    }; // box_css
+  } // if (md)
+  else {
+    box_css = {
+      A: { ...box_css.A, gridRow: '1 / 2',  gridColumn: '1 / -1',  },
+      B: { ...box_css.B, gridRow: '2 / 3',  gridColumn: '1 / 2',   },
+      C: { ...box_css.C, gridRow: '2 / 3',  gridColumn: '2 / 3',   },
+    }; // box_css
+  }
+
+
   return (
     <>
-      <Box sx={{ flexGrow: 1, mb: 4 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={6} md={8}>
-            <Item>A</Item>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Item>B</Item>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Item>C</Item>
-          </Grid>
-          <Grid item xs={6} md={8}>
-            <Item>D</Item>
-          </Grid>
-        </Grid>
-      </Box>
-
       <Box
         sx={{ 
           display: 'grid',
@@ -80,9 +90,9 @@ export default () => {
           gridTemplateRows: 'repeat(2, 1fr)',
         }}
       >
-        <Box sx={{ ...box_css, gridRow: '1 / -1', gridColumn: '1 / 2', background: 'deepskyblue' }}>A</Box>
-        <Box sx={{ ...box_css, gridRow: '1 / 2', gridColumn: '2 / 3',  background: 'tomato'  }}>B</Box>
-        <Box sx={{ ...box_css, gridRow: '2 / 3', gridColumn: '2 / 3',  background: 'darkorchid'  }}>C</Box>
+        <Item sx={ box_css.A }>A</Item>
+        <Item sx={ box_css.B }>B</Item>
+        <Item sx={ box_css.C }>C</Item>
       </Box>
 
       <Box
@@ -90,19 +100,12 @@ export default () => {
           display: 'flex',
         }}
       >
-        <Box sx={{
-          border: 'solid white 2px',
-          p: 1,
-        }}>
-          <p>xs Text: { sizes.xs }</p>
-          <p>xs: { JSON.stringify(xs) }</p>
-        </Box>
 
         <Box sx={{
           border: 'solid white 2px',
           p: 1,
         }}>
-          <p>sm Text: { sizes.sm }</p>
+          {/* <p>sm Text: { sizes.sm }</p> */}
           <p>sm: { JSON.stringify(sm) }</p>
         </Box>
 
@@ -110,7 +113,7 @@ export default () => {
           border: 'solid white 2px',
           p: 1,
         }}>
-          <p>md Text: { sizes.md }</p>
+          {/* <p>md Text: { sizes.md }</p> */}
           <p>md: { JSON.stringify(md) }</p>
         </Box>
 
@@ -118,7 +121,7 @@ export default () => {
           border: 'solid white 2px',
           p: 1,
         }}>
-          <p>lg Text: { sizes.lg }</p>
+          {/* <p>lg Text: { sizes.lg }</p> */}
           <p>lg: { JSON.stringify(lg) }</p>
         </Box>
 
@@ -126,7 +129,7 @@ export default () => {
           border: 'solid white 2px',
           p: 1,
         }}>
-          <p>xl Text: { sizes.xl }</p>
+          {/* <p>xl Text: { sizes.xl }</p> */}
           <p>xl: { JSON.stringify(xl) }</p>
         </Box>
       </Box>
