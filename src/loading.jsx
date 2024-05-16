b33ebda5a84ec887bf16a363e7a2d0cb98a7d9cb
joshdotjs <joshdotjs@gmail.com>
2024-05-16 12:15:44 -0500

    meta data

--- src/loading.jsx
+++ src/loading.jsx
@@ -8,23 +8,20 @@ import img from './assets/adobe-loading-animation--rounded-7dot-5px--dark.json';
 
 export default () => {
   return (
-    <div>
-      Loading...
-      <Player
-        autoplay
-        loop
-        src={img}
-        style={{
-          height: '300px',
-          width: '300px',
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)'
-        }}
-      >
-        <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-      </Player>
-    </div>
+    <Player
+      autoplay
+      loop
+      src={img}
+      style={{
+        height: '300px',
+        width: '300px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
+    >
+      <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+    </Player>
   );
 };
